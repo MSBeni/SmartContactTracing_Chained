58b9d7a1ba4f08bfafb04c2674704965067c7ccf
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-22 14:17:04 -0500

    Added the security handel 2

--- chainedSCT/authentication/security.py
+++ chainedSCT/authentication/security.py
@@ -6,8 +6,7 @@ def authenticate(id_, password):
     # print(AuthAcceptedUsers.fetch_All_authorized_IUP(93877848))
     # print(AuthAcceptedUsers.get_auth_user_by_id(93877848))
     user = AuthAcceptedUsers.get_auth_user_by_id(int(id_))
-    print("user: ", user)
-    if user and safe_str_cmp(user.password, password):
+    if user and safe_str_cmp(user['password'], password):
         return user
 
 
