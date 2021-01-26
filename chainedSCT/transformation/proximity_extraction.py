3c01b23e39b0af48be61573140633b71ac8a038d
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-26 13:45:13 -0500

    Added edits on the system

--- chainedSCT/transformation/proximity_extraction.py
+++ chainedSCT/transformation/proximity_extraction.py
@@ -1,7 +1,6 @@
 from .proximity import Proximity
 from ..extraction.user import User
 from ..extraction.locations import Location
-from ..extraction.database import CursorFromConnectionPool
 import numpy as np
 
 class ProximityCALC:
@@ -10,15 +9,9 @@ class ProximityCALC:
     def prox_calc(argument_handler):
         unique_contact_dates = set(Location.fetch_loc_dates()[0])
         unique_contact_dates = list(unique_contact_dates)
-        # print(unique_contact_dates[0][0])
         user_ids = User.load_all_ids_from_db()
         user_ids = user_ids[0]
-        # print(user_ids[0][0])
         # unique_users_in_this_date = set(Location.fetch_proximity_ids_by_date(unique_contact_dates[0][0])[0])
-        # print(unique_users_in_this_date)
-        #
-        # unique_users_in_this_date = set(Location.fetch_proximity_loc_by_date_id(unique_contact_dates[0], user_ids[0])[0])
-        # unique_users_in_this_date = Location.fetch_proximity_loc_by_date_id(unique_contact_dates[0][0], user_ids[0][0])
         # print(set(unique_users_in_this_date[0]))
         Proximity.create_proximity_table()
         for date in unique_contact_dates:
