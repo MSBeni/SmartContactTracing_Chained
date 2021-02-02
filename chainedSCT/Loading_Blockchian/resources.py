7a3a28bcf35cc9c0d08773dbc972eec94add3b48
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-01 20:55:33 -0500

    Added new class for infected contacts out of resource

--- chainedSCT/Loading_Blockchian/resources.py
+++ chainedSCT/Loading_Blockchian/resources.py
@@ -5,7 +5,6 @@ from uuid import uuid4
 from .node import Node
 from .blockchain import Blockchain
 from ..authentication.IUP_Definition import InfectedUsersPool
-from ..transformation.proximity import Proximity
 from ..authentication.authentication_IUP import AuthorizedUsers
 
 
@@ -45,20 +44,6 @@ class GetInfectedNodes(Resource):
         return {'infected users': Infected_nodes_}, 200
 
 
-class InfectedContacts:
-    @staticmethod
-    def infected_ids(user_id_):
-        Infected_nodes_ = Proximity.fetch_ids_in_close_proximity(user_id_)
-
-        unique_infected_Contact_List = []
-        for el in Infected_nodes_:
-            unique_infected_Contact_List.append(el[0])
-        unique_infected_Contact_List = list(set(unique_infected_Contact_List))
-        return unique_infected_Contact_List
-
-    @staticmethod
-    def infected_ids_before_date(user_id_, date_):
-        Infected_nodes_ = Proximity.fetch_ids_in_close_proximity_before_date(user_id_, date_)
 
 
 class GetInfectedNodeContacts(Resource):
